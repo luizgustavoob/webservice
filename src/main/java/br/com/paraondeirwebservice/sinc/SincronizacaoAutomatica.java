29b5c0870058bcb2d3825337140b9e5e921854e9
luizgustavoob <luizgustavobourscheit@gmail.com>
2017-02-09 22:23:18 -0200

    Alterar horário de execução

--- src/main/java/br/com/paraondeirwebservice/sinc/SincronizacaoAutomatica.java
+++ src/main/java/br/com/paraondeirwebservice/sinc/SincronizacaoAutomatica.java
@@ -16,7 +16,7 @@ public class SincronizacaoAutomatica {
 	
 	private static final String TIME_ZONE = "America/Sao_Paulo";
 
-	@Scheduled(cron = "0 */5 * * * *", zone = TIME_ZONE) //Sempre 1 da manhã 0 0 1 * * * 
+	@Scheduled(cron = "0 0 1 * * *", zone = TIME_ZONE) //Sempre 1 da manhã  
 	public void solicitarSincronizacao() throws Exception {
 		try {
 			controller.notificarSincronizacao();
